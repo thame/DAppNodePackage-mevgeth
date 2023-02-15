d1c12f7618d672df1fb4b74f9ff0f00aa4c8c4fb
Voss <voss@visnovalabs.io>
2023-02-15 07:12:17 -0600

    add lodestar

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,10 @@ case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_MAINNET" in
   echo "Using teku.dnp.dappnode.eth"
   JWT_PATH="/security/teku/jwtsecret.hex"
   ;;
+"lodestar.dnp.dappnode.eth")
+  echo "Using lodestar.dnp.dappnode.eth"
+  JWT_PATH="/security/lodestar/jwtsecret.hex"
+  ;;
 "nimbus.dnp.dappnode.eth")
   echo "Using nimbus.dnp.dappnode.eth"
   JWT_PATH="/security/nimbus/jwtsecret.hex"
